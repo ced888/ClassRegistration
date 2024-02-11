ff13a378ac235a2ce11ec47e8db843dd108a00d0
YoBear <brian.lin.blin@gmail.com>
2024-02-10 21:47:46 -0700

    Reverting Cart work I did

--- ClassRegistry/ClassRegistry/Form1.cs
+++ ClassRegistry/ClassRegistry/Form1.cs
@@ -3,31 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Configuration;
 
 
 namespace ClassRegistry
 {  
     public partial class Form1 : Form
     {
-        private string connectionString;
-
-        //variable to store the logged in student's ID
-        public int? loggedInStudentID = null;
-
-
         public Form1()
         {
             InitializeComponent();
-            connectionString = "Server=group-6.database.windows.net;Database=ClassRegistry;User Id=group6;Password=Test123456;";
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -76,51 +66,15 @@ namespace ClassRegistry
             this.sp_course_Sections_by_course_IDTableAdapter.Fill(this.classRegistryDataSet1.sp_course_Sections_by_course_ID, x);
         }
 
-        private void RefreshCartDataGridView()
-        {
-  
-            var tableAdapter = new ClassRegistryDataSet1TableAdapters.CartDataTable1Adapter();
-
-
-            var dataTable = new ClassRegistryDataSet1.CartDataTable1DataTable();
-
-            tableAdapter.Fill(dataTable, loggedInStudentID.Value); // Assuming loggedInStudentID is the parameter value
-
-            foreach (DataColumn column in dataTable.Columns)
-            {
-                Console.WriteLine($"Column Name: {column.ColumnName}, Data Type: {column.DataType}");
-            }
-
-
-            dataGridView_Cart.DataSource = dataTable;
-             
-        }
-
         private void btn_addToCart_Click(object sender, EventArgs e)
         {
             DataRowView row = (DataRowView)dataGridView_CourseSections.SelectedRows[0].DataBoundItem;
+            //Grabs the course_section id and course id integers
             int course_sectionID = (int)row.Row.ItemArray[6];
             int courseID = (int)row.Row.ItemArray[5];
-
-            // Insert into Cart table
-            string sql = $"INSERT INTO Cart (student_ID, course_section_ID) VALUES ({loggedInStudentID}, {course_sectionID})";
-
-            // Execute the SQL statement
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.ExecuteNonQuery();
-                }
-            }
-
-            RefreshCartDataGridView();
-
         }
 
-        
-        
+        public int? loggedInStudentID = null;
 
         private void loginButton_Click(object sender, EventArgs e)
         {
@@ -129,26 +83,17 @@ namespace ClassRegistry
             {
  
                 var studentsTableAdapter = new Students1TableAdapter();
-                var studentResults = studentsTableAdapter.FindByStudentID(studentID);
+                var studentExists = studentsTableAdapter.FindByStudentID(studentID);
 
-
-                if (studentResults != null && studentResults.Rows.Count > 0)
+                if (studentExists != null && studentExists is DataTable dataTable && dataTable.Rows.Count > 0)
                 {
                     loggedInStudentID = studentID;
-
-                    RefreshCartDataGridView();
-                    //Getting the Student's name, there's probably a better way
-                    var studentName = studentResults[0]["first_name"].ToString().Trim() + " "
-                        + studentResults[0]["last_name"].ToString().Trim();
-
-
                     MessageBox.Show("You have successfully logged in!");
                     loggedInLabel.Visible = true;
-                    loggedInLabel.Text = "Logged in as: " + studentID + " : " + studentName;
+                    loggedInLabel.Text = "Logged in as: " + studentID;
                     logOut.Visible = true;
                     loginButton.Visible = false;
                     iDField.ReadOnly = true;
-                    btn_addToCart.Enabled = true;
 
                 }
                 else
@@ -173,13 +118,7 @@ namespace ClassRegistry
             iDField.ReadOnly = false;
             iDField.Clear();
             loggedInStudentID = null;
-            btn_addToCart.Enabled = false;
 
-
-        }
-
-        private void dataGridView_Cart_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
 
         }
     }
